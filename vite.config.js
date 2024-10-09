4e60f2f25d65e7352f7868d39cfce2bd35006f6e
kroongft <51918951+kroongft@users.noreply.github.com>
2024-10-09 16:00:37 +0900

    chore: npm 배포를 위한 설정

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import * as path from "path";
 
 export default defineConfig({
-  plugins: [react()],
   build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.tsx"),
+      name: "doksi-ui",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`,
+    },
     rollupOptions: {
       external: ["react", "react-dom"], // React를 외부 의존성으로 설정
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+      },
+      banner: "use client",
+      interop: "auto",
+    },
+    commonjsOptions: {
+      esmExternals: ["react"],
     },
   },
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   server: {
     port: 3000,
   },
