a5044faf657d58edd57d7cf4d44fbc9a38fbdbb8
kroongft <51918951+kroongft@users.noreply.github.com>
2025-01-01 17:15:16 +0900

    chore: export DoksiGlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 export { default as Button } from './components/Button/Button'
 export { default as Icons } from './components/Icons/Icons'
 export { default as Badge } from './components/Badge/Badge'
+export { default as DoksiGlobalStyle } from './styles/DoksiGlobalStyle'
 
 export { doksiTheme } from './styles/theme'
 export { DoksiThemeProvider } from './DoksiThemeProvider'
