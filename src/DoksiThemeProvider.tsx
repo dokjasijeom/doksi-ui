08a7d131df489be9a2da0d75a79218cfd7da6cad
kroongft <51918951+kroongft@users.noreply.github.com>
2025-01-01 18:20:25 +0900

    chore: DoksiThemeProvider에 DoksiGlobalStyle 제거

--- src/DoksiThemeProvider.tsx
+++ src/DoksiThemeProvider.tsx
@@ -1,17 +1,11 @@
 import { ReactNode } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { doksiTheme } from './styles/theme'
-import DoksiGlobalStyle from './styles/DoksiGlobalStyle'
 
 type DoksiThemeProviderProps = {
   children: ReactNode
 }
 
 export const DoksiThemeProvider = ({ children }: DoksiThemeProviderProps) => {
-  return (
-    <ThemeProvider theme={doksiTheme}>
-      <DoksiGlobalStyle />
-      {children}
-    </ThemeProvider>
-  )
+  return <ThemeProvider theme={doksiTheme}>{children}</ThemeProvider>
 }
