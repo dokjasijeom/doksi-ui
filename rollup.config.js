bb9413c7487644119a3040800949264ca1b03612
kroongft <51918951+kroongft@users.noreply.github.com>
2025-01-05 23:08:41 +0900

    chore: rollup external 설정에 styled-components 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,25 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import babel from "@rollup/plugin-babel";
-import svgr from "@svgr/rollup";
-import typescript from "@rollup/plugin-typescript";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import { terser } from "rollup-plugin-terser";
-import copy from "rollup-plugin-copy";
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import babel from '@rollup/plugin-babel'
+import svgr from '@svgr/rollup'
+import typescript from '@rollup/plugin-typescript'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import { terser } from 'rollup-plugin-terser'
+import copy from 'rollup-plugin-copy'
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json')
 
 export default {
-  input: "src/index.tsx", // TypeScript의 진입점
+  input: 'src/index.tsx', // TypeScript의 진입점
   output: [
     {
       file: packageJson.main,
-      format: "cjs", // CommonJS 출력
+      format: 'cjs', // CommonJS 출력
       sourcemap: true,
     },
     {
       file: packageJson.module,
-      format: "esm", // ESModules 출력
+      format: 'esm', // ESModules 출력
       sourcemap: true,
     },
   ],
@@ -27,8 +27,8 @@ export default {
     copy({
       targets: [
         {
-          src: "src/components/Icons/icons/*.svg",
-          dest: "dist/components/Icons/icons",
+          src: 'src/components/Icons/icons/*.svg',
+          dest: 'dist/components/Icons/icons',
         },
       ],
     }),
@@ -36,13 +36,13 @@ export default {
     peerDepsExternal(), // peerDependencies를 번들에서 제외
     resolve(), // node_modules에서 모듈 로드
     commonjs(), // CommonJS 모듈 변환
-    typescript({ tsconfig: "./tsconfig.json" }), // TypeScript 처리
+    typescript({ tsconfig: './tsconfig.json' }), // TypeScript 처리
     babel({
-      exclude: "node_modules/**", // Babel이 node_modules를 제외하도록 설정
-      presets: ["@babel/preset-react"],
-      babelHelpers: "bundled",
+      exclude: 'node_modules/**', // Babel이 node_modules를 제외하도록 설정
+      presets: ['@babel/preset-react'],
+      babelHelpers: 'bundled',
     }),
     terser(), // 번들 압축 및 최적화
   ],
-  external: ["react", "react-dom"], // React와 ReactDOM을 외부 의존성으로 처리
-};
+  external: ['react', 'react-dom', 'styled-components'], // React와 ReactDOM을 외부 의존성으로 처리
+}
